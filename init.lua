910d0d658dce9792352232370196b1c5fc70eddb
mthgn <mhugon@particule-asso.org>
2023-12-27 21:43:39 +0100

    Premier commit de la config Neoviù

---
+++ init.lua
@@ -0,0 +1,16 @@
+print("che vuoi ?")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+vim.opt.clipboard = "unnamed"
+
+vim.opt.scrolloff = 999
