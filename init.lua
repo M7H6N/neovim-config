f382860e2d76584f3076ace83f6622251667f13c
mthgn <mhugon@particule-asso.org>
2023-12-28 08:52:49 +0100

    ajout de lazy et organisation de la config

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
-print("che vuoi ?")
+require("options")
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.wrap = false
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-
-vim.opt.clipboard = "unnamed"
-
-vim.opt.scrolloff = 999
+require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,26 @@
+        --LAZY.NVIM SETUP
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "rebelot/kanagawa.nvim",
+    {
+         "ellisonleao/gruvbox.nvim";
+         config = function()
+             vim.cmd.colorscheme("gruvbox")
+         end,
+    }
+    
+})
+
